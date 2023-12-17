7e276bd25e896086c3c32b592cef0ad6ef280ff4
aryanRiyal <aryan13.riyal@gmail.com>
2023-12-17 19:13:51 +0530

    basics - oops(const, references, 'this' keyword and operator overloading)

--- basics/oops/Fraction.cpp
+++ basics/oops/Fraction.cpp
@@ -4,13 +4,32 @@ class Fraction{
         int denominator;
 
     public:
+        Fraction() {
+
+        }
 
         Fraction(int numerator, int denominator){
             this->numerator = numerator;
             this->denominator = denominator;
         }
 
-        void print() {
+        int getNumerator() const {
+            return numerator;
+        }
+
+        int getDenominator() const {
+            return denominator;
+        }
+
+        void setNumerator(int n) {
+            numerator = n;
+        }
+
+        void setDenominator(int d) {
+            denominator = d;
+        }
+
+        void print() const {
             cout<<numerator<<"/"<<denominator<<endl;
         }
 
@@ -26,19 +45,72 @@ class Fraction{
             this->denominator = this->denominator/gcd;
         }
 
-        void add(Fraction const &f2) {
+        Fraction add(Fraction const &f2) {
             int lcm = denominator * f2.denominator;
             int x = lcm/denominator;
             int y = lcm/f2.denominator;
             int num = x * numerator + (y * f2.numerator);
+            Fraction fNew( num, lcm);
+            fNew.simplify();
+            return fNew;
+        }
+
+        Fraction multiply(Fraction const &f2) {
+            int num = numerator * f2.numerator;
+            int denom = denominator * f2.denominator;
+            Fraction fNew( num, denom);
+            fNew.simplify();
+            return fNew;
+        }
+
+        Fraction operator+(Fraction const &f2) const {            //First Argument is in 'this' keyword and second argument is in function argument
+            int lcm = denominator * f2.denominator;
+            int x = lcm/denominator;
+            int y = lcm/f2.denominator;
+            int num = x * numerator + (y * f2.numerator);
+            Fraction fNew( num, lcm);
+            fNew.simplify();
+            return fNew;
+        }
+
+        //Pre-increment
+        Fraction& operator++() {                                   //Fraction& because we are Returning by reference
+            numerator = numerator + denominator;
+            simplify();
+            return *this;
+        }
+
+        //Post-increment
+        Fraction operator++(int) {
+            Fraction fNew(numerator, denominator);
+            numerator = numerator + denominator;
+            simplify();
+            fNew.simplify();
+            return fNew;
+        }
+
+        Fraction& operator+=(Fraction const &f2) {
+            int lcm = denominator * f2.denominator;
+            int x = lcm / denominator;
+            int y = lcm / f2.denominator;
+
+            int num = x * numerator + (y * f2.numerator);
+
             numerator = num;
             denominator = lcm;
             simplify();
+            return *this;
         }
 
-        void multiply(Fraction const &f2) {
-            numerator = numerator * f2.numerator;
-            denominator = denominator * f2.denominator;
-            simplify();
+        Fraction operator*(Fraction const &f2) const {
+            int num = numerator * f2.numerator;
+            int denom = denominator * f2.denominator;
+            Fraction fNew( num, denom);
+            fNew.simplify();
+            return fNew;
+        }
+
+        bool operator==(Fraction const &f2) const {
+            return (numerator == f2.numerator && denominator == f2.denominator);
         }
 };
--- basics/oops/FractionUse.cpp
+++ basics/oops/FractionUse.cpp
@@ -7,25 +7,78 @@ int main() {
     Fraction f1(10,2);
     Fraction f2(15,4);
 
+    if(f1 == f2) {
+        cout<<"Equal"<<endl;
+    }
+    else {
+        cout<<"Not Equal"<<endl;
+    }
+
     cout<<"Fraction f1 = ";
     f1.print();
     cout<<"Fraction f2 = ";
     f2.print();
     cout<<endl;
 
-    cout<<"adding f1 and f2: calling f1.add(f2)"<<endl;
-    f1.add(f2);
+    cout<<"adding f1 and f2: f3 = f1.add(f2)"<<endl;
+    Fraction f3 = f1.add(f2);
     cout<<"Fraction f1 = ";
     f1.print();
     cout<<"Fraction f2 = ";
     f2.print();
+    cout<<"Fraction f3 = ";
+    f3.print();
+
+    cout<<endl<<"Operator Overloading: operator+"<<endl;
+    f1 = f2 + f3;
+    cout<<"f1 = f2 + f3;"<<endl<<"f1 = ";
+    f1.print();
+
+    cout<<endl<<"Pre-increment: operator++"<<endl;
+    f3 = ++f1;
+    cout<<"f3 = ++f1;"<<endl<<"f1 = ";
+    f1.print();
+    cout<<"f3 = ";
+    f3.print();
+    f1 = ++(++f3);
+    cout<<"f1 = ++(++f3);"<<endl<<"f3 = ";
+    f3.print();
+    cout<<"f1 = ";
+    f1.print();
+
+    cout<<endl<<"Post-increment: operator++"<<endl;
+    f3 = f1++;
+    cout<<"f3 = f1++;"<<endl<<"f1 = ";
+    f1.print();
+    cout<<"f3 = ";
+    f3.print();
     cout<<endl;
 
-    cout<<"multiplying f1 and f2: calling f1.multiply(f2)"<<endl;
-    f1.multiply(f2);
+    (f1 += f2) += f2;
+    cout<<"(f1 += f2) += f2;"<<endl<<"f1 = ";
+    f1.print();
+    cout<<"f2 = ";
+    f2.print();
+    cout<<endl<<endl;
+
+    cout<<"multiplying f1 and f2: f3 = f1.multiply(f2)"<<endl;
+    f3 = f1.multiply(f2);
     cout<<"Fraction f1 = ";
     f1.print();
     cout<<"Fraction f2 = ";
     f2.print();
+    cout<<"Fraction f3 = ";
+    f3.print();
+    cout<<endl<<"Operator Overloading: operator*"<<endl;
+    f1 = f2 * f3;
+    cout<<"f1 = f2 * f3;"<<endl<<"f1 = ";
+    f1.print();
+    cout<<endl;
+
+    Fraction const f4( 3, 11);
+    cout<<"Fraction f4 = ";
+    f4.print();
+    cout<<"f4.getNumerator() = "<<f4.getNumerator()<<", f4.getDenominator() = "<<f4.getDenominator()<<endl;
+    
     return 0;
 }
